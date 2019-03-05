ac4476fd826c05909e58ffe746c851f9cca19212
theortman <metwesttieno@gmail.com>
2019-03-05 01:52:32 -0800

    some physics implemented

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -383,11 +383,15 @@ function (_Phaser$Scene) {
   _inherits(Level1, _Phaser$Scene);
 
   function Level1() {
+    var _this;
+
     _classCallCheck(this, Level1);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Level1).call(this, {
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Level1).call(this, {
       key: _CST.CST.SCENES.LEVEL1
     }));
+    var harun;
+    return _this;
   } //preloading all the scenes cuz its a level and we dont wanna wait for enginge to grab them
 
 
@@ -441,51 +445,64 @@ function (_Phaser$Scene) {
       console.log("Loaded Level1");
       this.add.image(0, 0, "Level1Scene").setOrigin(0).setDepth(0); //setting the lvl 
 
-      this.idleHarun = this.add.sprite(100, 600, "IDLE").setScale(1).play("idle"); //initializing sprite so that it can be used
-
-      this.runRightHarun = this.add.sprite(this.idleHarun.x, this.idleHarun.y + 25, "RUN").setScale(1).play("runRight").setVisible(false);
-      this.idleLeftHarun = this.add.sprite(this.idleHarun.x, this.idleHarun.y + 25, "RUN").setScale(1).play("idleLeft").setVisible(false);
-      this.runLeftHarun = this.add.sprite(this.idleLeftHarun.x, this.idleLeftHarun.y + 25, "RUN").setScale(1).play("runLeft").setVisible(false);
-      this.slashHarun = this.add.sprite(325, 575, "RUN").setScale(1).play("slash").setVisible(false);
-      window.runRightHarun = this.runRightHarun; //making it global so we can use it outside this shithole
-
-      window.idleHarun = this.idleHarun;
+      /* this.harun = this.physics.add.sprite(200,600, "IDLE").setScale(1).play("idle"); //initializing sprite so that it can be used
+        this.runRightHarun = this.physics.add.sprite(this.harun.x,this.harun.y  + 25, "RUN").setScale(1).play("runRight").setVisible(false);
+        this.idleLeftHarun = this.physics.add.sprite(this.harun.x,this.harun.y + 25, "RUN").setScale(1).play("idleLeft").setVisible(false);
+        this.runLeftHarun = this.physics.add.sprite(this.idleLeftHarun.x,this.idleLeftHarun.y +25, "RUN").setScale(1).play("runLeft").setVisible(false);
+        this.slashHarun = this.physics.add.sprite(325,575, "RUN").setScale(1).play("slash").setVisible(false);
+            window.runRightHarun = this.runRightHarun; //making it global so we can use it outside this shithole
+      window.harun = this.harun;
+      
+      
+      
       window.idleLeftHarun = this.idleLeftHarun;
       window.runLeftHarun = this.runLeftHarun;
-      window.slashHarun = this.slashHarun;
-      this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
-      this.cameras.main.startFollow(this.idleHarun); //THIS ONE LINE DOES THE FUCKING CAMERA THING IM GONNA KILL MYSELF
+      window.slashHarun = this.slashHarun; */
 
-      this.cameras.main.setFollowOffset(-350, 200);
+      this.harun = this.physics.add.sprite(200, 600, "idle");
+      this.harun.setCollideWorldBounds(true);
+      this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
+      this.cameras.main.startFollow(this.harun); //THIS ONE LINE DOES THE FUCKING CAMERA THING IM GONNA KILL MYSELF
+
+      this.cameras.main.setFollowOffset(-300, 200);
     }
   }, {
     key: "update",
     value: function update(delta) {
       var isRunning = false; //this is where the magic happens, D works FINE need help with logic for A, too tired atm
 
-      if (this.keyboard.D.isDown === true) {
+      if (this.keyboard.D.isDown) {
         console.log("d is down");
-        this.idleHarun.flipX = false;
+        /* this.harun.flipX = false;
         this.runRightHarun.flipX = false;
-        this.idleHarun.setVisible(false);
+          this.harun.setVisible(false);
         this.runRightHarun.setVisible(true);
-        this.idleHarun.x = this.idleHarun.x + 2;
-        this.runRightHarun.x = this.idleHarun.x;
-        this.runRightHarun.play("runRight", true);
+          this.harun.x = this.harun.x + 2;
+        this.runRightHarun.x = this.harun.x;
+          this.runRightHarun.play("runRight", true); */
+
+        this.harun.setVelocityX(160);
+        this.harun.flipX = false;
+        this.harun.setY = this.harun.y + 25;
+        this.harun.anims.play("runRight", true);
       } else if (this.keyboard.A.isDown === true) {
-        this.idleHarun.flipX = true;
+        /* this.harun.flipX = true;
         this.runRightHarun.flipX = true;
-        this.idleHarun.setVisible(false);
+          this.harun.setVisible(false);
         this.runRightHarun.setVisible(true);
-        this.idleHarun.x = this.idleHarun.x - 2;
-        this.runRightHarun.x = this.idleHarun.x;
-        this.runRightHarun.play("runRight", true);
-        this.isflipped = true;
+          this.harun.x = this.harun.x - 2;
+        this.runRightHarun.x = this.harun.x;
+          this.runRightHarun.play("runRight", true);
+        this.isflipped = true; */
         console.log("a is down");
+        this.harun.setVelocityX(-160);
+        this.harun.flipX = true;
+        this.harun.setY = this.harun.y + 25;
+        this.harun.anims.play("runRight", true);
       } else {
-        this.idleHarun.setVisible(true);
-        this.runRightHarun.setVisible(false);
-        console.log("d is up");
+        this.harun.setVelocityX(0);
+        this.harun.anims.play("idle", true);
+        console.log("Im Idle");
       }
     }
   }, {
@@ -551,7 +568,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58478" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54710" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
