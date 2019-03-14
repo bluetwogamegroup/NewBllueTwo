2c22f3e04d714de783e7492810db941cf0cb6e5f
theortman <metwesttieno@gmail.com>
2019-03-14 14:16:23 -0700

    try 6

--- dist/CST.00b51bf9.js
+++ dist/CST.00b51bf9.js
@@ -149,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54328" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59325" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
