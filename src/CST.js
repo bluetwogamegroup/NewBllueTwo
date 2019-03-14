37aa2e33e761d140f9cecf5f3d5331fc066bd8aa
Luka Gajic <lukagajic@gmail.com>
2019-03-14 02:08:10 -0700

    added Key to Open the ability to jump up the castle

---
+++ src/CST.js
@@ -0,0 +1,9 @@
+export const CST = {
+    SCENES: {
+        LOAD: "LOAD",
+        MENU: "MENU",
+        LEVEL1: "LEVEL1",
+        PAUSE: "PAUSE",
+        OPTIONS: "OPTIONS"
+    } //THIS IS WHERE WE SET UP SCENES
+}
